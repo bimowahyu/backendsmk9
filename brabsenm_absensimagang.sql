169fe683a48837684b917b37cccf0271802c4228
bimowahyu <ronibimo5@gmail.com>
2025-02-06 11:33:40 +0700

    update sql

--- brabsenm_absensimagang.sql
+++ brabsenm_absensimagang.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Waktu pembuatan: 20 Jan 2025 pada 17.59
+-- Waktu pembuatan: 06 Feb 2025 pada 11.27
 -- Versi server: 10.6.17-MariaDB-cll-lve
 -- Versi PHP: 8.1.31
 
@@ -45,11 +45,8 @@ CREATE TABLE `absensis` (
 
 INSERT INTO `absensis` (`id`, `tgl_absensi`, `jam_masuk`, `jam_keluar`, `foto_masuk`, `foto_keluar`, `createdAt`, `updatedAt`, `userId`) VALUES
 (1, '2025-01-15', '16:16:10', '16:17:56', 'testsiswa-2025-01-15-masuk.png', 'testsiswa-2025-01-15-keluar.png', '2025-01-15 16:16:10', '2025-01-15 16:17:56', 2),
-(2, '2025-01-16', '11:04:56', '12:45:33', 'testsiswa-2025-01-16-masuk.jpg', 'testsiswa-2025-01-16-keluar.jpg', '2025-01-16 11:04:56', '2025-01-16 12:45:33', 2),
-(3, '2025-01-16', '13:40:15', '16:01:25', 'testsiswa2-2025-01-16-masuk.jpg', 'testsiswa2-2025-01-16-keluar.jpg', '2025-01-16 13:40:15', '2025-01-16 16:01:25', 3),
-(4, '2025-01-17', '10:35:23', '12:55:14', 'siswaakl-2025-01-17-masuk.jpg', 'siswaakl-2025-01-17-keluar.jpg', '2025-01-17 10:35:23', '2025-01-17 12:55:14', 5),
-(5, '2025-01-17', '11:16:57', NULL, 'testsiswa-2025-01-17-masuk.jpg', NULL, '2025-01-17 11:16:57', '2025-01-17 11:16:57', 2),
-(6, '2025-01-20', '10:19:26', NULL, 'testsiswa-2025-01-20-masuk.jpg', NULL, '2025-01-20 10:19:26', '2025-01-20 10:19:26', 2);
+(2, '2025-01-16', '11:00:00', '12:40:00', 'testsiswa-2025-01-16-masuk.jpg', 'testsiswa-2025-01-16-keluar.jpg', '2025-01-16 11:04:56', '2025-02-01 04:00:24', 2),
+(4, '2025-01-17', '10:35:23', '12:55:14', 'siswaakl-2025-01-17-masuk.jpg', 'siswaakl-2025-01-17-keluar.jpg', '2025-01-17 10:35:23', '2025-01-17 12:55:14', 5);
 
 -- --------------------------------------------------------
 
@@ -70,7 +67,9 @@ CREATE TABLE `jurusans` (
 
 INSERT INTO `jurusans` (`id`, `namaJurusan`, `createdAt`, `updatedAt`) VALUES
 (1, 'X AKL1', '2025-01-15 10:49:03', '2025-01-15 10:49:03'),
-(2, 'X RPL1', '2025-01-15 10:49:19', '2025-01-15 10:49:19');
+(2, 'X RPL1', '2025-01-15 10:49:19', '2025-01-15 10:49:19'),
+(4, 'XII PM1', '2025-01-20 19:13:59', '2025-01-20 19:14:14'),
+(5, 'XII AP1', '2025-01-23 20:16:39', '2025-01-23 20:16:44');
 
 -- --------------------------------------------------------
 
@@ -81,7 +80,7 @@ INSERT INTO `jurusans` (`id`, `namaJurusan`, `createdAt`, `updatedAt`) VALUES
 CREATE TABLE `laporans` (
   `id` int(11) NOT NULL,
   `tgl_pembuatan` date NOT NULL,
-  `foto_laporan` varchar(255) NOT NULL,
+  `foto_laporan` varchar(255) DEFAULT NULL,
   `keterangan` longtext DEFAULT NULL,
   `createdAt` datetime NOT NULL,
   `updatedAt` datetime NOT NULL,
@@ -94,7 +93,6 @@ CREATE TABLE `laporans` (
 
 INSERT INTO `laporans` (`id`, `tgl_pembuatan`, `foto_laporan`, `keterangan`, `createdAt`, `updatedAt`, `userId`) VALUES
 (1, '2025-01-15', 'testsiswa-2025-01-15-1737143926466.png', 'Laporan absensi karyawan bulan Januari', '2025-01-15 11:17:23', '2025-01-18 02:58:46', 2),
-(2, '2025-01-15', 'testsiswa-2025-01-15-1736914660405.png', 'Laporan absensi karyawan bulan Januari', '2025-01-15 11:17:40', '2025-01-15 11:17:40', 2),
 (4, '2025-01-15', 'testsiswa-2025-01-15-1737199718246.jpg', 'Laporan absensi karyawan bulan Januari', '2025-01-15 11:19:00', '2025-01-18 18:28:38', 2),
 (5, '2025-01-15', 'testsiswa-2025-01-15-1736917280050.png', 'magang harike 2', '2025-01-15 12:01:20', '2025-01-15 12:01:20', 2),
 (6, '2025-01-15', 'testsiswa-2025-01-15-1736917430247.png', 'magang harike 2 dengan laporan pembelajaran baru', '2025-01-15 12:03:50', '2025-01-15 12:03:50', 2),
@@ -106,7 +104,12 @@ INSERT INTO `laporans` (`id`, `tgl_pembuatan`, `foto_laporan`, `keterangan`, `cr
 (12, '2025-01-17', 'siswaakl-2025-01-17-1737093302274.png', 'Test debug', '2025-01-17 12:55:02', '2025-01-17 12:55:02', 5),
 (13, '2025-01-17', 'testsiswa-2025-01-17-1737093784781.jpg', 'Testupload update', '2025-01-17 13:03:04', '2025-01-18 02:56:30', 2),
 (14, '2025-01-18', 'testsiswa-2025-01-18-1737225388290.jpg', 'testing1update test\r\n', '2025-01-18 03:12:54', '2025-01-19 01:36:28', 2),
-(15, '2025-01-20', 'testsiswa-2025-01-20-1737368619394.png', 'Teks adalah rangkaian kata atau kalimat yang memiliki struktur dan tata bahasa tertentu. Teks dapat disusun secara lisan atau tulisan. \r\nTujuan teks adalah untuk menyampaikan informasi, gagasan, atau pesan kepada pembaca atau pendengar. Teks juga dapat menghibur, mengajar, atau meyakinkan pembaca. \r\nJenis-jenis teks:\r\nTeks narasi, yaitu teks yang digunakan untuk menceritakan cerita atau narasi. Contohnya novel, cerita pendek, dongeng, dan autobiografi. \r\nTeks prosedur, yaitu teks yang berisi panduan, langkah-langkah, atau tutorial untuk melakukan sesuatu. \r\nTeks argumentasi, yaitu teks yang memuat penjelasan tentang pendapat atau fakta berdasarkan sudut pandang penulis. \r\nTeks deskripsi, yaitu teks yang memaparkan atau menggambarkan sesuatu menggunakan kata-kata dengan jelas dan terperinci. \r\nTeks eksposisi, yaitu teks yang mengandung serangkaian informasi dan wawasan yang disampaikan secara singkat, padat, dan jelas. \r\nTeks normatif, yaitu teks yang isinya ditulis berdasarkan sebuah peraturan, norma-norma, atau peraturan yang berlaku.', '2025-01-20 17:23:39', '2025-01-20 17:23:39', 2);
+(15, '2025-01-20', 'testsiswa-2025-01-20-1737368619394.png', 'Test\r\n', '2025-01-20 17:23:39', '2025-02-04 12:10:24', 2),
+(16, '2025-01-20', 'testsiswa-2025-01-20-1737374360884.png', 'Test download pdf', '2025-01-20 18:59:20', '2025-02-04 12:10:38', 2),
+(17, '2025-01-23', 'testsiswa-2025-01-23-1737633356180.png', 'Update pagination', '2025-01-23 18:55:56', '2025-01-23 18:55:56', 2),
+(18, '2025-01-26', 'testsiswa-2025-01-26-1737829986077.jpg', 'Test paginate', '2025-01-26 01:33:06', '2025-01-26 01:33:06', 2),
+(19, '2025-01-26', 'testsiswa-2025-01-26-1737829999703.jpg', 'Testing update paginate', '2025-01-26 01:33:19', '2025-01-26 01:33:35', 2),
+(20, '2025-02-01', 'uploadexcel-2025-02-01-1738357139652.jpg', 'buat baru setelah update', '2025-02-01 03:58:59', '2025-02-01 03:58:59', 12);
 
 -- --------------------------------------------------------
 
@@ -127,24 +130,8 @@ CREATE TABLE `Sessions` (
 --
 
 INSERT INTO `Sessions` (`sid`, `expires`, `data`, `createdAt`, `updatedAt`) VALUES
-('-dejYC9C5u-R7Wq_h6Dymbv1kqw5k9SK', '2025-01-21 15:10:19', '{\"cookie\":{\"originalMaxAge\":null,\"expires\":null,\"secure\":false,\"httpOnly\":true,\"path\":\"/\"},\"_garbage\":\"Mon Jan 20 2025 15:10:19 GMT+0700 (Western Indonesia Time)\",\"userId\":4,\"userRole\":\"superadmin\"}', '2025-01-20 14:15:23', '2025-01-20 15:10:19'),
-('2FEoCobIfIjvIUoS-xcvKi41LUiKEICz', '2025-01-21 10:22:30', '{\"cookie\":{\"originalMaxAge\":null,\"expires\":null,\"secure\":false,\"httpOnly\":true,\"path\":\"/\"},\"_garbage\":\"Mon Jan 20 2025 10:22:30 GMT+0700 (Western Indonesia Time)\",\"userId\":2,\"userRole\":\"siswa\"}', '2025-01-20 10:21:56', '2025-01-20 10:22:30'),
-('5RLyJAtk7JdUU7x0ULbEtJU1rdgY4_y_', '2025-01-21 17:23:12', '{\"cookie\":{\"originalMaxAge\":null,\"expires\":null,\"secure\":false,\"httpOnly\":true,\"path\":\"/\"},\"_garbage\":\"Mon Jan 20 2025 17:23:12 GMT+0700 (Western Indonesia Time)\"}', '2025-01-20 17:23:12', '2025-01-20 17:23:12'),
-('9AJ2PX3bzEGvwFI-sUy_PSz31Eavqfy_', '2025-01-21 08:21:36', '{\"cookie\":{\"originalMaxAge\":null,\"expires\":null,\"secure\":false,\"httpOnly\":true,\"path\":\"/\"},\"_garbage\":\"Mon Jan 20 2025 08:21:36 GMT+0700 (Western Indonesia Time)\",\"userId\":2,\"userRole\":\"siswa\"}', '2025-01-20 08:21:33', '2025-01-20 08:21:36'),
-('AVKdEunrP0iAmH78GFwDvaDrFaB_DCNT', '2025-01-21 03:37:15', '{\"cookie\":{\"originalMaxAge\":null,\"expires\":null,\"secure\":false,\"httpOnly\":true,\"path\":\"/\"},\"_garbage\":\"Mon Jan 20 2025 03:37:15 GMT+0700 (Western Indonesia Time)\"}', '2025-01-20 03:37:15', '2025-01-20 03:37:15'),
-('CEEMy4G0hRhd37vY72xmkGpc2dpxA0MJ', '2025-01-21 17:10:18', '{\"cookie\":{\"originalMaxAge\":null,\"expires\":null,\"secure\":false,\"httpOnly\":true,\"path\":\"/\"},\"_garbage\":\"Mon Jan 20 2025 17:10:18 GMT+0700 (Western Indonesia Time)\",\"userId\":2,\"userRole\":\"siswa\"}', '2025-01-20 17:07:30', '2025-01-20 17:10:18'),
-('DkEJ9WLKvqJiVhiuifksgYIjzRO08Kv9', '2025-01-21 17:14:24', '{\"cookie\":{\"originalMaxAge\":null,\"expires\":null,\"secure\":false,\"httpOnly\":true,\"path\":\"/\"},\"_garbage\":\"Mon Jan 20 2025 17:14:24 GMT+0700 (Western Indonesia Time)\",\"userId\":2,\"userRole\":\"siswa\"}', '2025-01-20 17:12:52', '2025-01-20 17:14:24'),
-('Eeb4jO7JLsAxnPFhNTLbo9aKx-i84bQ3', '2025-01-21 15:37:11', '{\"cookie\":{\"originalMaxAge\":null,\"expires\":null,\"secure\":false,\"httpOnly\":true,\"path\":\"/\"},\"_garbage\":\"Mon Jan 20 2025 15:37:11 GMT+0700 (Western Indonesia Time)\"}', '2025-01-20 15:37:11', '2025-01-20 15:37:11'),
-('eJVNpNIYgFG42yPe713ScsP0otp9R-xi', '2025-01-21 17:23:12', '{\"cookie\":{\"originalMaxAge\":null,\"expires\":null,\"secure\":false,\"httpOnly\":true,\"path\":\"/\"},\"_garbage\":\"Mon Jan 20 2025 17:23:12 GMT+0700 (Western Indonesia Time)\"}', '2025-01-20 17:23:12', '2025-01-20 17:23:12'),
-('EX2_mjV6L-JtaGWBk92_sWdIkC27k4wD', '2025-01-21 01:56:19', '{\"cookie\":{\"originalMaxAge\":null,\"expires\":null,\"secure\":false,\"httpOnly\":true,\"path\":\"/\"},\"_garbage\":\"Mon Jan 20 2025 01:56:19 GMT+0700 (Western Indonesia Time)\"}', '2025-01-20 01:56:19', '2025-01-20 01:56:19'),
-('F6Me1TFBLVP8PDWn_NYttyx6UDKQFgnY', '2025-01-21 16:13:47', '{\"cookie\":{\"originalMaxAge\":null,\"expires\":null,\"secure\":false,\"httpOnly\":true,\"path\":\"/\"},\"_garbage\":\"Mon Jan 20 2025 16:13:47 GMT+0700 (Western Indonesia Time)\"}', '2025-01-20 16:13:47', '2025-01-20 16:13:47'),
-('hGKPbAm-SkJDiOm7KyFPZnkrtLdD5nE_', '2025-01-21 12:52:53', '{\"cookie\":{\"originalMaxAge\":null,\"expires\":null,\"secure\":false,\"httpOnly\":true,\"path\":\"/\"},\"_garbage\":\"Mon Jan 20 2025 12:52:53 GMT+0700 (Western Indonesia Time)\"}', '2025-01-20 12:52:53', '2025-01-20 12:52:53'),
-('i_aAE_S5HL0gWCmubXubbrASveFM5pWP', '2025-01-21 17:24:51', '{\"cookie\":{\"originalMaxAge\":null,\"expires\":null,\"secure\":false,\"httpOnly\":true,\"path\":\"/\"},\"_garbage\":\"Mon Jan 20 2025 17:24:51 GMT+0700 (Western Indonesia Time)\"}', '2025-01-20 17:24:51', '2025-01-20 17:24:51'),
-('lx7GSvpMxsF1bEXkCalQXlxuMoWS6RMu', '2025-01-21 10:01:31', '{\"cookie\":{\"originalMaxAge\":null,\"expires\":null,\"secure\":false,\"httpOnly\":true,\"path\":\"/\"},\"_garbage\":\"Mon Jan 20 2025 10:01:31 GMT+0700 (Western Indonesia Time)\"}', '2025-01-20 10:01:31', '2025-01-20 10:01:31'),
-('MKWjY_NwcLR3Y08Mg9F_ilna1AJYjPaC', '2025-01-21 12:52:53', '{\"cookie\":{\"originalMaxAge\":null,\"expires\":null,\"secure\":false,\"httpOnly\":true,\"path\":\"/\"},\"_garbage\":\"Mon Jan 20 2025 12:52:53 GMT+0700 (Western Indonesia Time)\"}', '2025-01-20 12:52:53', '2025-01-20 12:52:53'),
-('NdClacCQGowznkoCcGzvbuh2Ne2sQUNc', '2025-01-21 12:52:53', '{\"cookie\":{\"originalMaxAge\":null,\"expires\":null,\"secure\":false,\"httpOnly\":true,\"path\":\"/\"},\"_garbage\":\"Mon Jan 20 2025 12:52:53 GMT+0700 (Western Indonesia Time)\"}', '2025-01-20 12:52:53', '2025-01-20 12:52:53'),
-('oBDZVnmvsDkN8EVQOpT0jamw5DL6bLeH', '2025-01-21 17:23:12', '{\"cookie\":{\"originalMaxAge\":null,\"expires\":null,\"secure\":false,\"httpOnly\":true,\"path\":\"/\"},\"_garbage\":\"Mon Jan 20 2025 17:23:12 GMT+0700 (Western Indonesia Time)\"}', '2025-01-20 17:23:12', '2025-01-20 17:23:12'),
-('wY6NwE0oX9lwUlrF9O0pBQ9O1h9h_wcE', '2025-01-20 19:22:01', '{\"cookie\":{\"originalMaxAge\":null,\"expires\":null,\"secure\":false,\"httpOnly\":true,\"path\":\"/\"},\"_garbage\":\"Sun Jan 19 2025 19:22:01 GMT+0700 (Western Indonesia Time)\"}', '2025-01-19 19:22:01', '2025-01-19 19:22:01');
+('qPh63uRcLBVbUV-NMEKBGzRyaeSnXPlh', '2025-02-06 09:02:37', '{\"cookie\":{\"originalMaxAge\":600000,\"expires\":\"2025-02-06T02:02:37.849Z\",\"secure\":false,\"httpOnly\":true,\"path\":\"/\"},\"_garbage\":\"Thu Feb 06 2025 08:52:37 GMT+0700 (Western Indonesia Time)\"}', '2025-02-06 08:52:37', '2025-02-06 08:52:37'),
+('t1JfXS604Xd8Rc4T7snFnGJyQo8YP9Wt', '2025-02-06 09:02:39', '{\"cookie\":{\"originalMaxAge\":600000,\"expires\":\"2025-02-06T02:02:39.791Z\",\"secure\":false,\"httpOnly\":true,\"path\":\"/\"},\"_garbage\":\"Thu Feb 06 2025 08:52:39 GMT+0700 (Western Indonesia Time)\"}', '2025-02-06 08:52:39', '2025-02-06 08:52:39');
 
 -- --------------------------------------------------------
 
@@ -169,11 +156,19 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`id`, `name`, `username`, `password`, `role`, `jurusanId`, `createdAt`, `updatedAt`) VALUES
 (1, 'testadmin', 'testadmin', '$argon2id$v=19$m=65536,t=3,p=4$nQLuBkGbkOY0YidcO4cb8Q$jQCcZphSkAyh82BG5skOz6FcU9tyO9HETuk+3GS+JcM', 'admin', 1, '2025-01-15 10:48:55', '2025-01-16 14:29:34'),
-(2, 'testsiswa', 'testsiswa', '$argon2id$v=19$m=65536,t=3,p=4$KwgzNvWoEwZfjKc/FxPrgA$9RVUFwgcwWWrmbjgdJpLpULYjqjdU+l4oUA45kHixgY', 'siswa', 1, '2025-01-15 10:49:06', '2025-01-16 19:51:09'),
+(2, 'Siswa', 'testsiswa', '$argon2id$v=19$m=65536,t=3,p=4$pJm5LfM4NvgOLySx+MmQMQ$Vp+8uSiA/BYR0ipM5DAw7RQV1RyAWxn0s1H8hPMLyrw', 'siswa', 1, '2025-01-15 10:49:06', '2025-02-01 01:50:54'),
 (3, 'testsiswa2', 'testsiswa2', '$argon2id$v=19$m=65536,t=3,p=4$afi1Yu68zrTLTmcwXNIFUw$V2iFlXuqrD6TGsGggPBMy4eUWRc+/V3bp9v/IVuUw6A', 'siswa', 2, '2025-01-16 12:45:56', '2025-01-16 12:51:33'),
 (4, 'superadmin', 'superadmin', '$argon2id$v=19$m=65536,t=3,p=4$FMUcGRSYGcLYNKSZzYVDGg$W+Php7LTuWWEuKLKLeaFQzSE8B+yZLs4T5+qm8YBfTU', 'superadmin', NULL, '2025-01-16 14:29:53', '2025-01-16 14:29:53'),
 (5, 'siswaakl', 'siswaakl', '$argon2id$v=19$m=65536,t=3,p=4$4f4KGgMB3JdXdw2pwnkCSQ$OIK+KqaTi/u0Rzp0RdL9iHunX0xQCO0Ssczab3NthX4', 'siswa', 1, '2025-01-17 08:49:39', '2025-01-17 08:49:39'),
-(6, 'adminrpl', 'adminrpl', '$argon2id$v=19$m=65536,t=3,p=4$q9iXw3JjOT6Qvxf/AJoCuw$otpR1/rSsgMSDLCquumVLDlSCjomiNblmjwmz+IDe4Y', 'admin', 2, '2025-01-17 08:52:04', '2025-01-17 08:52:04');
+(6, 'adminrpl', 'adminrpl', '$argon2id$v=19$m=65536,t=3,p=4$q9iXw3JjOT6Qvxf/AJoCuw$otpR1/rSsgMSDLCquumVLDlSCjomiNblmjwmz+IDe4Y', 'admin', 2, '2025-01-17 08:52:04', '2025-01-17 08:52:04'),
+(7, 'Adminpm1', 'Adminpm1', '$argon2id$v=19$m=65536,t=3,p=4$F0S4W9AFEoanuBocdRpb/A$4X7Ze9CuVxCbVyxJLl4CvO9LBdB2tGIpf/rdBlzYM6A', 'admin', 4, '2025-01-20 19:14:56', '2025-01-20 19:14:56'),
+(8, 'Superadmin2', 'Superadmin2', '$argon2id$v=19$m=65536,t=3,p=4$RfGh+utEM/b1TuZTHeTYJw$W9lBaaAbHF5wd+uMzrdR+IJ9Im8MX11FZKOCqz2TRUU', 'superadmin', NULL, '2025-01-20 19:15:14', '2025-01-20 19:15:14'),
+(9, 'Siswapm1', 'Siswapm1', '$argon2id$v=19$m=65536,t=3,p=4$7mNanClwAVdYeLzv4kk2mA$n17ZXWXD8sp8XytzTlR0LQfpfRy4C+OYk6MtE3Hdlew', 'siswa', 4, '2025-01-20 19:16:00', '2025-01-20 19:16:00'),
+(10, 'adminap', 'adminap', '$argon2id$v=19$m=65536,t=3,p=4$eN1y4E5kqIRR0FzES5vtQA$wRXlj7iuHgD5lxn3zZN34jooCKpoYY2CjQ4s52wqCyg', 'admin', 5, '2025-01-23 20:17:01', '2025-01-23 20:17:01'),
+(11, 'siswaap', 'siswaap', '$argon2id$v=19$m=65536,t=3,p=4$tsdKHX6mpnT2fdHuq6BBmw$zXK4PV8890VvV57S2r9SFyPWQaChwovvP/FQAJUTTmQ', 'siswa', 5, '2025-01-23 20:17:18', '2025-01-23 20:17:18'),
+(12, 'uploadexcel', 'uploadexcel', '$argon2id$v=19$m=65536,t=3,p=4$fkqRud2hmQhOLqUa211CKQ$7vqFYi5kYu/El/R3z+3XEtmfV+u9PqT/vjTaxgh/R4Y', 'siswa', 1, '2025-02-01 03:58:04', '2025-02-01 03:58:04'),
+(13, 'siswauploadexcel', 'siswauploadexcel', '$argon2id$v=19$m=65536,t=3,p=4$p9sV+vI0txIFXnLneI30yQ$aru9hpkSvMOXZU/tr5eFzWDU7iL8ZbW5MFIL2P3URfQ', 'siswa', 1, '2025-02-01 03:58:04', '2025-02-01 03:58:04'),
+(14, 'bimo', 'bimo', '$argon2id$v=19$m=65536,t=3,p=4$OrseNNZAOM1h9DqcTNCmlA$uoMo6xTUPTf5VMUunK8Dsubk7xT7iQxZNQsFjFxf5eU', 'siswa', 1, '2025-02-01 03:59:48', '2025-02-01 03:59:48');
 
 --
 -- Indexes for dumped tables
@@ -220,25 +215,25 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT untuk tabel `absensis`
 --
 ALTER TABLE `absensis`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT untuk tabel `jurusans`
 --
 ALTER TABLE `jurusans`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT untuk tabel `laporans`
 --
 ALTER TABLE `laporans`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- AUTO_INCREMENT untuk tabel `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
